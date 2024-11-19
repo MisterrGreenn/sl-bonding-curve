222a90b415a27236f47973a765a5976631dfafb6
Pham Tu <tubackkhoa@gmail.com>
2024-11-19 16:11:47 +0700

    refractor to test-suite for counter_test

--- programs/anchor-counter/tests/counter_test.rs
+++ programs/anchor-counter/tests/counter_test.rs
@@ -10,137 +10,35 @@ use solana_sdk::{account::Account, signature::Keypair, signer::Signer, transacti
 
 #[tokio::test]
 async fn test_initialize() {
-    let SetUpTest {
-        validator,
-        user,
-        counter_pda,
-    } = SetUpTest::new();
+    let mut test_suite = SetUpTest::new().await;
+    test_suite.initialize().await.unwrap();
 
-    let mut context = validator.start_with_context().await;
-
-    let init_ix = Instruction {
-        program_id: anchor_counter::ID,
-        accounts: anchor_counter::accounts::Initialize {
-            counter: counter_pda,
-            user: user.pubkey(),
-            system_program: system_program::ID,
-        }
-        .to_account_metas(None),
-        data: anchor_counter::instruction::Initialize {}.data(),
-    };
-
-    let init_tx = Transaction::new_signed_with_payer(
-        &[init_ix],
-        Some(&user.pubkey()),
-        &[&user],
-        context.last_blockhash,
-    );
-
-    context
-        .banks_client
-        .process_transaction(init_tx)
-        .await
-        .unwrap();
-
-    let counter: anchor_counter::Counter = load_and_deserialize(context, counter_pda).await;
+    let counter: anchor_counter::Counter = test_suite.load_and_deserialize().await;
 
     assert_eq!(counter.count, 0);
 }
 
 #[tokio::test]
 async fn test_increment() {
-    let SetUpTest {
-        validator,
-        user: _,
-        counter_pda,
-    } = SetUpTest::new();
+    let mut test_suite = SetUpTest::new().await;
+    test_suite.initialize().await.unwrap();
 
-    let mut context = validator.start_with_context().await;
+    test_suite.increment().await.unwrap();
 
-    let init_ix = Instruction {
-        program_id: anchor_counter::ID,
-        accounts: anchor_counter::accounts::Initialize {
-            counter: counter_pda,
-            user: context.payer.pubkey(),
-            system_program: system_program::ID,
-        }
-        .to_account_metas(None),
-        data: anchor_counter::instruction::Initialize {}.data(),
-    };
-
-    let increment_ix = Instruction {
-        program_id: anchor_counter::ID,
-        accounts: anchor_counter::accounts::Increment {
-            counter: counter_pda,
-            user: context.payer.pubkey(),
-        }
-        .to_account_metas(None),
-        data: anchor_counter::instruction::Increment {}.data(),
-    };
-
-    let init_increment_tx = Transaction::new_signed_with_payer(
-        &[init_ix, increment_ix],
-        Some(&context.payer.pubkey()),
-        &[&context.payer],
-        context.last_blockhash,
-    );
-
-    let _res = context
-        .banks_client
-        .process_transaction(init_increment_tx)
-        .await;
-
-    let counter: anchor_counter::Counter = load_and_deserialize(context, counter_pda).await;
+    let counter: anchor_counter::Counter = test_suite.load_and_deserialize().await;
 
     assert_eq!(counter.count, 1);
 }
 
 #[tokio::test]
 async fn test_double_increment() -> anyhow::Result<()> {
-    let SetUpTest {
-        validator,
-        user,
-        counter_pda,
-    } = SetUpTest::new();
+    let mut test_suite = SetUpTest::new().await;
+    test_suite.initialize().await.unwrap();
 
-    let mut context = validator.start_with_context().await;
+    test_suite.increment().await.unwrap();
+    test_suite.increment().await.unwrap();
 
-    let init_ix = Instruction {
-        program_id: anchor_counter::ID,
-        accounts: anchor_counter::accounts::Initialize {
-            counter: counter_pda,
-            user: user.pubkey(),
-            system_program: system_program::ID,
-        }
-        .to_account_metas(None),
-        data: anchor_counter::instruction::Initialize {}.data(),
-    };
-
-    let increment_ix = Instruction {
-        program_id: anchor_counter::ID,
-        accounts: anchor_counter::accounts::Increment {
-            counter: counter_pda,
-            user: user.pubkey(),
-        }
-        .to_account_metas(None),
-        data: anchor_counter::instruction::Increment {}.data(),
-    };
-
-    let increment_ix_2 = increment_ix.clone();
-
-    let init_increment_tx = Transaction::new_signed_with_payer(
-        &[init_ix, increment_ix, increment_ix_2],
-        Some(&user.pubkey()),
-        &[&user],
-        context.last_blockhash,
-    );
-
-    let _res = context
-        .banks_client
-        .process_transaction(init_increment_tx)
-        .await;
-
-    let counter: anchor_counter::Counter = load_and_deserialize(context, counter_pda).await;
+    let counter: anchor_counter::Counter = test_suite.load_and_deserialize().await;
 
     assert_eq!(counter.count, 2);
 
@@ -149,23 +47,16 @@ async fn test_double_increment() -> anyhow::Result<()> {
 
 #[tokio::test]
 async fn test_bogus_counter_acct() -> anyhow::Result<()> {
-    let SetUpTest {
-        validator,
-        user,
-        counter_pda,
-    } = SetUpTest::new();
+    let mut test_suite = SetUpTest::new().await;
+    test_suite.initialize().await.unwrap();
 
-    let mut context = validator.start_with_context().await;
-
-    initialize(&mut context, &user, &counter_pda).await?;
-
-    //let (bogus_pda, _) = Pubkey::find_program_address(&[b"counter_bad"], &anchor_counter::ID);
+    let (bogus_pda, _) = Pubkey::find_program_address(&[b"counter_bad"], &anchor_counter::ID);
 
     let increment_ix = Instruction {
         program_id: anchor_counter::ID,
         accounts: anchor_counter::accounts::Increment {
-            counter: user.pubkey(), /*bogus_pda*/
-            user: user.pubkey(),
+            counter: bogus_pda,
+            user: test_suite.user.pubkey(),
         }
         .to_account_metas(None),
         data: anchor_counter::instruction::Increment {}.data(),
@@ -173,12 +64,16 @@ async fn test_bogus_counter_acct() -> anyhow::Result<()> {
 
     let increment_tx = Transaction::new_signed_with_payer(
         &[increment_ix],
-        Some(&user.pubkey()),
-        &[&user],
-        context.last_blockhash,
+        Some(&test_suite.user.pubkey()),
+        &[&test_suite.user],
+        test_suite.ctx.last_blockhash,
     );
 
-    let res = context.banks_client.process_transaction(increment_tx).await;
+    let res = test_suite
+        .ctx
+        .banks_client
+        .process_transaction(increment_tx)
+        .await;
 
     assert!(res.is_err());
 
@@ -188,14 +83,14 @@ async fn test_bogus_counter_acct() -> anyhow::Result<()> {
 /// Struct set up to hold the validator, an optional user account, and the counter PDA.
 /// Use SetUpTest::new() to create a new instance.
 pub struct SetUpTest {
-    pub validator: ProgramTest,
+    pub ctx: ProgramTestContext,
     pub user: Keypair,
     pub counter_pda: Pubkey,
 }
 
 /// Returns the validator, an optional funded user account, and the counter PDA
 impl SetUpTest {
-    pub fn new() -> Self {
+    pub async fn new() -> Self {
         //Both of these work
 
         // let mut validator = ProgramTest::default();
@@ -215,57 +110,72 @@ impl SetUpTest {
 
         //get the counter PDA -- uses the same seed we used in the anchor program
         let (counter_pda, _) = Pubkey::find_program_address(&[b"counter"], &anchor_counter::ID);
-
+        let ctx = validator.start_with_context().await;
         Self {
-            validator,
+            ctx,
             user,
             counter_pda,
         }
     }
-}
 
-///Function that initializes the counter account
-///Useful for testing things you want to fail but need to initialize the counter account first
-pub async fn initialize(
-    ctx: &mut ProgramTestContext,
-    user: &Keypair,
-    counter_pda: &Pubkey,
-) -> anyhow::Result<()> {
-    let init_ix = Instruction {
-        program_id: anchor_counter::ID,
-        accounts: anchor_counter::accounts::Initialize {
-            counter: *counter_pda,
-            user: user.pubkey(),
-            system_program: system_program::ID,
-        }
-        .to_account_metas(None),
-        data: anchor_counter::instruction::Initialize {}.data(),
-    };
+    pub async fn process_transaction(&mut self, intructions: &[Instruction]) -> anyhow::Result<()> {
+        let init_tx = Transaction::new_signed_with_payer(
+            intructions,
+            Some(&self.ctx.payer.pubkey()),
+            &[&self.ctx.payer],
+            self.ctx.last_blockhash,
+        );
 
-    let init_tx = Transaction::new_signed_with_payer(
-        &[init_ix],
-        Some(&user.pubkey()),
-        &[&user],
-        ctx.last_blockhash,
-    );
+        self.ctx.banks_client.process_transaction(init_tx).await?;
 
-    ctx.banks_client.process_transaction(init_tx).await.unwrap();
+        // move to next block
+        self.ctx.warp_forward_force_reward_interval_end()?;
 
-    Ok(())
-}
+        Ok(())
+    }
 
-/// Fetch the account from the ProgramTestContext and deserialize it.
-/// Taken from the MarginFi Github tests: https://github.com/mrgnlabs/marginfi-v2/blob/main/test-utils/src/test.rs#L468
-pub async fn load_and_deserialize<T: AccountDeserialize>(
-    mut ctx: ProgramTestContext,
-    address: Pubkey,
-) -> T {
-    let account = ctx
-        .banks_client
-        .get_account(address)
-        .await
-        .unwrap() //unwraps the Result into an Option<Account>
-        .unwrap(); //unwraps the Option<Account> into an Account
+    ///Function that initializes the counter account
+    ///Useful for testing things you want to fail but need to initialize the counter account first
+    pub async fn initialize(&mut self) -> anyhow::Result<()> {
+        let init_ix = Instruction {
+            program_id: anchor_counter::ID,
+            accounts: anchor_counter::accounts::Initialize {
+                counter: self.counter_pda,
+                user: self.ctx.payer.pubkey(),
+                system_program: system_program::ID,
+            }
+            .to_account_metas(None),
+            data: anchor_counter::instruction::Initialize {}.data(),
+        };
 
-    T::try_deserialize(&mut account.data.as_slice()).unwrap()
+        self.process_transaction(&[init_ix]).await
+    }
+
+    pub async fn increment(&mut self) -> anyhow::Result<()> {
+        let increment_ix = Instruction {
+            program_id: anchor_counter::ID,
+            accounts: anchor_counter::accounts::Increment {
+                counter: self.counter_pda,
+                user: self.ctx.payer.pubkey(),
+            }
+            .to_account_metas(None),
+            data: anchor_counter::instruction::Increment {}.data(),
+        };
+
+        self.process_transaction(&[increment_ix]).await
+    }
+
+    /// Fetch the account from the ProgramTestContext and deserialize it.
+    /// Taken from the MarginFi Github tests: https://github.com/mrgnlabs/marginfi-v2/blob/main/test-utils/src/test.rs#L468
+    pub async fn load_and_deserialize<T: AccountDeserialize>(&mut self) -> T {
+        let account = self
+            .ctx
+            .banks_client
+            .get_account(self.counter_pda)
+            .await
+            .unwrap() //unwraps the Result into an Option<Account>
+            .unwrap(); //unwraps the Option<Account> into an Account
+
+        T::try_deserialize(&mut account.data.as_slice()).unwrap()
+    }
 }
